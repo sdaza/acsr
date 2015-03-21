2d20b2feadea4b1e3afd081b63b28c19aee9911f
sdaza <sebastian.daza@gmail.com>
2015-03-21 15:25:52 -0500

    update

---
+++ R/acsr.R
@@ -0,0 +1,11 @@
+#' acsr: A package with wrapped functions to manage ACS files.
+#'
+#' The foo package provides three categories of important functions:
+#' foo, bar and baz.
+#' 
+#' @section Foo functions:
+#' The foo functions ...
+#'
+#' @docType package
+#' @name acsr
+NULL
