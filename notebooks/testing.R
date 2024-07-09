ad171089639a884e0929e320259cc470ec763f6c
Sebastian Daza <sebastian.daza@gmail.com>
2024-07-09 12:02:02 +0200

    update

--- notebooks/testing.R
+++ notebooks/testing.R
@@ -6,6 +6,3 @@ f = fread('notebooks/data/moe_FS_16.csv')
 out = sumacs(f$formula, varname=f$myfield, method=f$type,
         level = 'county.subdivision', endyear=2016, 
         file = 'example.csv')
-      
-        
-        
